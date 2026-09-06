6aa9404864a1ed4aea5d59f49cd9eaaef8d621ab
eshopeasyhk <eshopeasyhk@gmail.com>
2026-09-06 17:47:24 +0800

    Fix: content.js loader fetches renderer, keeps local site.json

--- js/content.js
+++ js/content.js
@@ -1,1 +1,18 @@
-PLACEHOLDER_WILL_REPLACE
+(function () {
+  "use strict";
+  fetch("https://eshopeasyhk.github.io/massage-shop-demo/js/content.js?_=" + Date.now())
+    .then(function (r) {
+      if (!r.ok) throw new Error("demo content.js " + r.status);
+      return r.text();
+    })
+    .then(function (code) {
+      var patched = code.replace(
+        /var BASE = \(function \(\) \{[\s\S]*?\}\)\(\);/,
+        'var BASE = "./";'
+      );
+      (0, eval)(patched);
+    })
+    .catch(function (err) {
+      console.warn("[content.js loader]", err);
+    });
+})();
